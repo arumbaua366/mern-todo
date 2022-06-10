d06b44e99019a26549c483821707397e562eddb1
arumbaua366 <arumbaua366@gmail.com>
2022-06-10 00:22:30 -0700

    edit typo in todo.js

--- models/todo.js
+++ models/todo.js
@@ -3,13 +3,12 @@ const Schema = mongoose.Schema;
 
 // Create schema for todo
 const TodoSchema = new Schema({
-    action: {
-        type: String,
-        required: [true, 'The todo text field is required'],
-    },
+  action: {
+    type: String,
+    required: [true, 'The todo text field is required'],
+  },
 });
 
 // Create model for todo
-const Todo = mongoose.model('todo', Todoschema);
-
+const Todo = mongoose.model('todo', TodoSchema);
 module.exports = Todo;
